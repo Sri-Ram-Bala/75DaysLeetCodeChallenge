7d566c2edb85e0dd5f3150702b6f025f5d30d165
Sriram Balamurugan <205185791+Sri-Ram-Bala@users.noreply.github.com>
2026-03-21 19:27:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,10 +1,11 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        hole=0
         for i in range (len(nums)):
-            if nums[i]!=0:
-                nums[hole],nums[i]=nums[i],nums[hole]
-                hole+=1
+            if nums[i]==0:
+                zero = nums.pop(i)
+                nums.append(zero)
+
+                
 
 
 
